9474e307c676b6c0d5934ed99265d5bae986441d
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2024-02-16 14:00:29 -0600

    using useEffect for animation

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -2,6 +2,7 @@ import { useLoaderData } from "@remix-run/react";
 import { getAllPosts } from "../models/posts.server";
 import avatar from "../../public/img/me.jpeg";
 import { dateformat } from "../utils/dateFormat";
+import { useEffect } from "react";
 
 export function meta() {
   return [{ title: "RicardoGmz - WebDeveloper" }];
@@ -13,53 +14,54 @@ export async function loader() {
   return posts;
 }
 
-if (typeof document === "undefined") {
-} else {
-  let eyes = document.querySelectorAll(".eye");
-  let eyeRect = eyes[0].getBoundingClientRect();
-  let container = document.querySelector(".eyes-container");
-
-  let containerRect = container.getBoundingClientRect();
-
-  document.addEventListener("mousemove", eyesFollow, false);
-
-  function eyesFollow(e) {
-    requestAnimationFrame(() => {
-      let xPos = e.pageX;
-      let yPos = e.pageY;
-
-      let xDiff = eyeRect.x + eyeRect.width / 2 - xPos;
-      let yDiff = eyeRect.y + eyeRect.height / 2 - yPos;
-
-      let angle = (Math.atan2(yDiff, xDiff) * 180) / Math.PI;
-
-      container.style.setProperty("--eyeAngle", angle.toFixed(2) + "deg");
-
-      let mouseXRelativeToContainer =
-        xPos - containerRect.x - containerRect.width / 2;
-      let mouseYRelativeToContainer =
-        yPos - containerRect.y - containerRect.height / 2;
-
-      let containerXAngle =
-        60 * (mouseXRelativeToContainer / window.innerWidth);
-      let containerYAngle =
-        -1 * 60 * (mouseYRelativeToContainer / window.innerHeight);
-
-      container.style.setProperty(
-        "--xAngle",
-        containerXAngle.toFixed(2) + "deg"
-      );
-      container.style.setProperty(
-        "--yAngle",
-        containerYAngle.toFixed(2) + "deg"
-      );
-    });
-  }
-}
-
 export default function Index() {
   const posts = useLoaderData().data;
 
+  useEffect(() => {
+    if (typeof document === "undefined") {
+    } else {
+      let eyes = document.querySelectorAll(".eye");
+      let eyeRect = eyes[0].getBoundingClientRect();
+      let container = document.querySelector(".eyes-container");
+
+      let containerRect = container.getBoundingClientRect();
+
+      document.addEventListener("mousemove", eyesFollow, false);
+
+      function eyesFollow(e) {
+        requestAnimationFrame(() => {
+          let xPos = e.pageX;
+          let yPos = e.pageY;
+
+          let xDiff = eyeRect.x + eyeRect.width / 2 - xPos;
+          let yDiff = eyeRect.y + eyeRect.height / 2 - yPos;
+
+          let angle = (Math.atan2(yDiff, xDiff) * 180) / Math.PI;
+
+          container.style.setProperty("--eyeAngle", angle.toFixed(2) + "deg");
+
+          let mouseXRelativeToContainer =
+            xPos - containerRect.x - containerRect.width / 2;
+          let mouseYRelativeToContainer =
+            yPos - containerRect.y - containerRect.height / 2;
+
+          let containerXAngle =
+            60 * (mouseXRelativeToContainer / window.innerWidth);
+          let containerYAngle =
+            -1 * 60 * (mouseYRelativeToContainer / window.innerHeight);
+
+          container.style.setProperty(
+            "--xAngle",
+            containerXAngle.toFixed(2) + "deg"
+          );
+          container.style.setProperty(
+            "--yAngle",
+            containerYAngle.toFixed(2) + "deg"
+          );
+        });
+      }
+    }
+  }, []);
   return (
     <main>
       {posts.map((post) => (
